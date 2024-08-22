1e12ea453d28d088ad6c0b99ca1d06382482f2b9
Robert Nancka <robirtpl@outlook.com>
2024-08-22 17:51:10 +0200

    Add Database Context.

---
+++ CartSync.Infrastructure/CartSyncDbContext.cs
@@ -0,0 +1,46 @@
+﻿using CartSync.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CartSync.Infrastructure;
+
+/// <summary>
+/// CartSync Database Context.
+/// </summary>
+public class CartSyncDbContext : DbContext
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CartSyncDbContext"/> class.
+    /// </summary>
+    public CartSyncDbContext() : base()
+    {
+        
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CartSyncDbContext"/> class.
+    /// </summary>
+    public CartSyncDbContext(DbContextOptions<CartSyncDbContext> options) : base(options)
+    {
+        
+    }
+
+    /// <summary>
+    /// Gets or sets Item Categories Database Set.
+    /// </summary>
+    public DbSet<ItemCategoryEntity> ItemCategories { get; set; }
+
+    /// <summary>
+    /// Gets or sets Users Database Set.
+    /// </summary>
+    public DbSet<UserEntity> Users { get; set; }
+
+    /// <summary>
+    /// Gets or sets Lists Database Set.
+    /// </summary>
+    public DbSet<ListEntity> Lists { get; set; }
+
+    /// <summary>
+    /// Gets or sets Items Database Set.
+    /// </summary>
+    public DbSet<ItemEntity> Items { get; set; }
+}
